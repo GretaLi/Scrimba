1d8dcc4136ca13ae64ee47b513811a2ca21db943
Greta (Hsinju) <106577649+GretaLi@users.noreply.github.com>
2023-11-22 18:15:13 +0800

    Update vite.config.js

--- travel-journal/vite.config.js
+++ travel-journal/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Scrimba/travel-journal/",
+  base: "/scrimba/travel-journal/",
   build: {
     outDir: "dist/travel-journal",
   },

ea01800a959d4edbf09c540b0b5165440429d653
GretaLi <hsinju1993@gmail.com>
2023-11-22 16:53:30 +0800

    Updates

--- airbnb-clone/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navbar() {
-  return (
-    <nav className="nav">
-      <img src="../images/airbnb-logo.png" className="nav--logo" />
-    </nav>
-  );
-}

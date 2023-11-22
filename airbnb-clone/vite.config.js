b11ff0c3bcb90f80c39bf757fb3538d8eb54399d
GretaLi <hsinju1993@gmail.com>
2023-11-22 16:50:18 +0800

    Update Vite configuration for GitHub Pages

--- airbnb-clone/vite.config.js
+++ airbnb-clone/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/scrimba/airbnb-clone/",
+  base: "/Scrimba/airbnb-clone/",
   build: {
-    outDir: "dist/airbnb-clone",
+    outDir: "dist",
   },
 });
